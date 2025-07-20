a8a5984877264885a73f54aefe0528c8078f79ee
Alivin <wangyongjie@mediinfo.com.cn>
2025-07-20 20:21:15 +0800

    fix: vercel.

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, { dev, isServer }) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
